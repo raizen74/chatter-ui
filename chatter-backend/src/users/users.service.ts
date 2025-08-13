5d1112a697d6dedf131bb9462ffb960403bb33ad
David Galera <raizen74@gmail.com>
2025-08-14 00:13:12 +0200

    GraphQL users service

--- chatter-backend/src/users/users.service.ts
+++ chatter-backend/src/users/users.service.ts
@@ -1,32 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-constructor(private readonly usersRepository: UsersRepository) {
+  constructor(private readonly usersRepository: UsersRepository) {}
 
-}
+  async create(createUserInput: CreateUserInput) {
+    return this.usersRepository.create({
+      ...createUserInput,
+      password: await this.hashPassword(createUserInput.password),
+    });
+  }
 
-
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+  private async hashPassword(password: string) {
+    return bcrypt.hash(password, 10);
   }
 
   async findAll() {
     return this.usersRepository.find({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(_id: string) {
+    return this.usersRepository.findOne({ _id });
   }
 
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
+  async update(_id: string, updateUserInput: UpdateUserInput) {
+    const updateData: any = { ...updateUserInput };
+    if (updateUserInput.password) {
+      updateData.password = await this.hashPassword(updateUserInput.password);
+    }
+    return this.usersRepository.findOneAndUpdate({ _id }, { $set: updateData });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(_id: string) {
+    return this.usersRepository.findOneAndDelete({ _id });
   }
 }
