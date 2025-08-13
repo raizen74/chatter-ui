bb9a79430de4c7f0fb2f51afede7b8585196e6bd
David Galera <raizen74@gmail.com>
2025-08-13 16:00:03 +0200

    GraphQL and Apollo server

---
+++ chatter-backend/src/common/database/database.module.ts
@@ -0,0 +1,17 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+
+@Module({
+    imports: [MongooseModule.forRootAsync({
+        useFactory: (configService: ConfigService) => (
+            {
+                uri: configService.get('MONGODB_URI'),
+            }
+        ),
+        inject: [ConfigService],
+    })],
+})
+export class DatabaseModule {
+  // This module can be used to encapsulate database-related providers and configurations
+}
