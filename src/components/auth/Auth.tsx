e94ca57f345950ee5eaf73b02e35b7b0221cbadd
David Galera <raizen74@gmail.com>
2025-08-13 12:57:57 +0200

    React UI first cut

---
+++ src/components/auth/Auth.tsx
@@ -0,0 +1,50 @@
+import { Button, Stack, TextField } from "@mui/material";
+import React, { useState } from "react";
+
+interface AuthProps {
+  // Define any props if needed
+  submitLabel: string;
+  onSubmit: (credentials: { email: string; password: string }) => Promise<void>;
+  children: React.ReactNode;
+}
+
+const Auth = ({ submitLabel, onSubmit, children }: AuthProps) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Stack
+      spacing={3}
+      sx={{
+        height: "100vh",
+        maxWidth: {
+          xs: "70%",
+          md: "30%",
+        },
+        margin: "0 auto",
+        justifyContent: "center",
+      }}
+    >
+      <TextField
+        type='email'
+        label='Email'
+        variant='outlined'
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <TextField
+        type='password'
+        label='Password'
+        variant='outlined'
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <Button variant='contained' onClick={() => onSubmit({ email, password })}>
+        {submitLabel}
+      </Button>
+      {children}
+    </Stack>
+  );
+};
+
+export default Auth;
