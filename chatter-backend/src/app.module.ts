c2ec4fba30c99d17ea3bfd92197b7c3f4faab55d
David Galera <raizen74@gmail.com>
2025-08-14 11:57:20 +0200

    nestjs-pino LoggerModule for local development

--- chatter-backend/src/app.module.ts
+++ chatter-backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import * as Joi from 'joi';
+import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './common/database/database.module';
@@ -13,14 +14,36 @@ import { UsersModule } from './users/users.module';
       isGlobal: true,
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
-    }),
+      }),
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
     DatabaseModule,
-    UsersModule
+    UsersModule,
+    LoggerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production';
+
+        return {
+          pinoHttp: {
+            transport: isProduction
+              ? undefined
+              : {
+                  target: 'pino-pretty',
+                  options: {
+                    singleLine: true,
+                    // colorize: true,
+                    // translateTime: 'SYS:standard',
+                  },
+                },
+            level: isProduction ? 'info' : 'debug',
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
     // Other modules can be imported here
   ],
   controllers: [AppController],
--- chatter-backend/src/main.ts
+++ chatter-backend/src/main.ts
@@ -1,12 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {bufferLogs: true}); // bufferLogs to ensure logs are captured before the app starts
   app.useGlobalPipes(
     new ValidationPipe()
   );
-  await app.listen(process.env.PORT ?? 3000);
+  app.useLogger(app.get(Logger));
+  const configService = app.get(ConfigService);
+  await app.listen(configService.getOrThrow<string>('PORT', '3000')); // Default to 3000 if PORT is not set
 }
 bootstrap();
